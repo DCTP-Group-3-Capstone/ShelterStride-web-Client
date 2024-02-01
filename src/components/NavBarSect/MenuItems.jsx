628e06add507d5a5eb32985cd415b9e44c8460e5
Adaobilynda1234 <adaobilynda1234@gmail.com>
2024-02-01 08:21:11 +0100

    added navbar

---
+++ src/components/NavBarSect/MenuItems.jsx
@@ -0,0 +1,13 @@
+//
+export const MenuItems = [
+  {
+    title: "Rent a home",
+    path: "/Rent",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Buy a home",
+    path: "/Buy",
+    cName: "dropdown-link",
+  },
+];
