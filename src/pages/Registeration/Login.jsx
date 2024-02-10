d9a61a0b9313790fa24a9d75ff1df57422808a0b
Becky <elizabethbecky17@gmail.com>
2024-02-10 10:33:28 +0100

    updatted frontend to include Becky branch and master

--- src/pages/Registeration/Login.jsx
+++ src/pages/Registeration/Login.jsx
@@ -5,11 +5,9 @@ import See from "../../assets/icon/See.svg";
 import UnSee from "../../assets/icon/UnSee.svg";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import Swal from 'sweetalert2'
-
-
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 //functions
 function Login() {
@@ -49,164 +47,165 @@ function Login() {
   //login handling
 
   const handleLoginApi = async (event) => {
-  const handleLoginApi = async (event) => {
-    event.preventDefault();
+    const handleLoginApi = async (event) => {
+      event.preventDefault();
 
-    //handle page naviagtion
-    //const history = useHistory();
+      //handle page naviagtion
+      //const history = useHistory();
 
-    // TOAST CONFIG
-    const Toast = Swal.mixin({
-      toast: true,
-      position: "top",
-      showConfirmButton: false,
-      timer: 3000,
-      timerProgressBar: true,
-      didOpen: (toast) => {
-        toast.onmouseenter = Swal.stopTimer;
-        toast.onmouseleave = Swal.resumeTimer;
-      }
-    });
-
-    if (!email || !password || password === " ") {
-      Toast.fire({
-        icon: "error",
-        title: "Fields can't be empty"
-      })
-      setError("Please enter both email and password");
-
-      return;
-    }
-
-    try {
-      const result = await axios.post("https://shelterstride.onrender.com/api/v1/login", {
-        email,
-        password
+      // TOAST CONFIG
+      const Toast = Swal.mixin({
+        toast: true,
+        position: "top",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer;
+          toast.onmouseleave = Swal.resumeTimer;
+        },
       });
-      // console.log(result);
 
-      const { status, data } = result;
+      if (!email || !password || password === " ") {
+        Toast.fire({
+          icon: "error",
+          title: "Fields can't be empty",
+        });
+        setError("Please enter both email and password");
 
-      if (status == 200) {
-        console.log("signed in");
+        return;
+      }
+
+      try {
+        const result = await axios.post(
+          "https://shelterstride.onrender.com/api/v1/login",
+          {
+            email,
+            password,
+          }
+        );
+        // console.log(result);
+
+        const { status, data } = result;
+
+        if (status == 200) {
+          console.log("signed in");
+
+          Swal.fire({
+            title: "Success",
+            text: "Signed up successfully",
+            icon: "success",
+            timer: 2000,
+          });
+
+          // Set token in local storage
+          localStorage.setItem("token", data.token);
+          // Redirect to profile page
+          navigate("/profile");
+        }
+      } catch (error) {
+        console.log(error);
+
+        let errorMessage = "An unexpected error occurred, please try again";
+
+        if (error.response && error.response.data) {
+          errorMessage = error.response.data.error || errorMessage;
+        }
 
         Swal.fire({
-          title: "Success",
-          text: "Signed up successfully",
-          icon: "success",
-          timer: 2000
+          title: "Error",
+          text: errorMessage,
+          icon: "error",
+          timer: 2000,
         });
-
-        // Set token in local storage
-        localStorage.setItem("token", data.token);
-        // Redirect to profile page
-        navigate("/profile")
       }
+    };
 
-    } catch (error) {
-      console.log(error);
+    return (
+      <>
+        <div className="Login-page">
+          <div className="logolabel">
+            <img
+              src="src\assets\images\ShelterStrideSideLogo.svg"
+              alt="Shelters Stride"
+            />
+          </div>
+          <div className="Login-container">
+            <h2> Welcome Back</h2>
+            <form onSubmit={handleLoginApi}>
+              <div className="Login-Box">
+                <div className="Login-form">
+                  <label htmlFor="email">Email Address</label>
+                  <div className="form-group">
+                    <img src={Email} alt="Email Icon" />
+                    <input
+                      type="email"
+                      id="email"
+                      value={email}
+                      onChange={handleEmail}
+                      onFocus={handleEmailFocus}
+                      placeholder="Enter your email"
+                    />
+                  </div>
+                  {!isValidEmail && (
+                    <p className="error-message">Invalid email address</p>
+                  )}
 
-      let errorMessage = "An unexpected error occurred, please try again";
+                  <label htmlFor="password">Password</label>
+                  <div className="form-group">
+                    <img src={lock} alt="Password Icon" />
+                    <input
+                      type={passwordVisible ? "text" : "password"}
+                      id="password"
+                      value={password}
+                      onChange={handlePassword}
+                      placeholder="Enter your password"
+                    />
+                    <div
+                      className="password-toggle"
+                      onClick={togglePasswordVisibility}
+                    >
+                      <img
+                        src={passwordVisible ? See : UnSee}
+                        alt="Toggle Password Visibility"
+                      />
+                    </div>
+                  </div>
 
-      if (error.response && error.response.data) {
-        errorMessage = error.response.data.error || errorMessage;
-      }
+                  <p className="forgot-password">
+                    <a
+                      href="/forgot-password"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Forgot Password?
+                    </a>
+                  </p>
+                  <p className="forgot-password">
+                    <a
+                      href="/forgot-password"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Forgot Password?
+                    </a>
+                  </p>
 
-      Swal.fire({
-        title: "Error",
-        text: errorMessage,
-        icon: "error",
-        timer: 2000
-      });
-
-    }
-  }
-
-
-  return (
-    <>
-      <div className="Login-page">
-        <div className="logolabel">
-          <img
-            src="src\assets\images\ShelterStrideSideLogo.svg"
-            alt="Shelters Stride"
-          />
-        </div>
-        <div className="Login-container">
-          <h2> Welcome Back</h2>
-          <form onSubmit={handleLoginApi}>
-            <div className="Login-Box">
-              <div className="Login-form">
-                <label htmlFor="email">Email Address</label>
-                <div className="form-group">
-                  <img src={Email}
-                    alt="Email Icon" />
-                  <input type="email"
-                    id="email"
-                    value={email}
-                    onChange={handleEmail}
-                    onFocus={handleEmailFocus}
-                    placeholder="Enter your email" />
-                </div>
-                {!isValidEmail && (
-                  <p className="error-message">Invalid email address</p>
-                )}
-
-
-
-                <label htmlFor="password">Password</label>
-                <div className="form-group">
-                  <img src={lock} alt="Password Icon" />
-                  <input
-                    type={passwordVisible ? "text" : "password"}
-                    id="password"
-                    value={password}
-                    onChange={handlePassword}
-                    placeholder="Enter your password"
-                  />
-                  <div
-                    className="password-toggle"
-                    onClick={togglePasswordVisibility}>
-                    <img src={passwordVisible ? See : UnSee} alt="Toggle Password Visibility" />
+                  <div className="button-group">
+                    <button type="submit">Sign in</button>
                   </div>
                 </div>
-
-                <p className="forgot-password">
-                  <a
-                    href="/forgot-password"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Forgot Password?
-                  </a>
-                </p>
-                <p className="forgot-password">
-                  <a
-                    href="/forgot-password"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Forgot Password?
-                  </a>
-                </p>
-
-                <div className="button-group">
-                  <button type="submit"
-                  >Sign in</button>
-                </div>
               </div>
-            </div>
-          </form>
-          </form>
-          <p className="Login-text">
-            New to ShelterStride ?{""}
-
-            <Link to="/createaccount">Sign up</Link>
-          </p>
+            </form>
+            <p className="Login-text">
+              New to ShelterStride ?{""}
+              <Link to="/createaccount">Sign up</Link>
+            </p>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  };
 }
 
 export default Login;
